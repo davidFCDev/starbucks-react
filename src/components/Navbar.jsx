dba0516ffbd183cfe4a52baf89c291bb60e6d745
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-30 16:16:55 +0200

    header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="flex">
+        <img src="https://cdn.freebiesupply.com/images/large/2x/starbucks-logo-png-transparent.png" alt="starbucks rewards logo" width={50}/>
+        <h3>MENU</h3>
+        <h3>REWARDS</h3>
+        <h3>GIFT CARDS</h3>
+      </div>
+      <div>
+        <h3>Find a Store</h3>
+        <a href="https://www.starbucks.com/account/signin">Sign In</a>
+        <button>Join Now</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
