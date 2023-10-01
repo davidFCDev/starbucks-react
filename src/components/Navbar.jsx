4b114215b02fe9c1f0d9e3b7379216172265fd20
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-01 20:31:55 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
+import { MdLocationOn } from "react-icons/md";
+
 const Navbar = () => {
   return (
     <div className="flex justify-between px-10 py-6">
-      <div className="flex gap-10 items-center font-bold">
+      <div className="flex gap-8 items-center font-bold h-full tracking-widest text-sm">
         <img
           src="https://cdn.freebiesupply.com/images/large/2x/starbucks-logo-png-transparent.png"
           alt="starbucks rewards logo"
@@ -11,16 +13,21 @@ const Navbar = () => {
         <h4>REWARDS</h4>
         <h4>GIFT CARDS</h4>
       </div>
-      <div className="flex gap-10 items-center font-semibold">
-        <h4>Find a Store</h4>
+      <div className="flex gap-16 items-center font-semibold text-sm">
+        <h4 className="flex gap-2 items-center">
+          <MdLocationOn />
+          Find a Store
+        </h4>
         <div className="flex gap-4">
           <a
             href="https://www.starbucks.com/account/signin"
-            className="border border-black rounded-3xl px-4 py-1"
+            className="border border-black rounded-3xl px-4 py-1 hover:bg-gray-200 transation-all"
           >
             Sign In
           </a>
-          <button className="border border-black bg-black text-white rounded-3xl px-4 py-1">Join Now</button>
+          <button className="border border-black bg-black text-white rounded-3xl px-4 py-1 hover:bg-gray-700 transation-all">
+            Join Now
+          </button>
         </div>
       </div>
     </div>
--- src/components/StickyNav.jsx
+++ src/components/StickyNav.jsx
@@ -2,7 +2,7 @@
 
 const StickyNav = () => {
   return (
-    <div className="bg-sb-dark-green px-10 py-3 sticky">
+    <div className="bg-sb-dark-green px-10 py-3 sticky text-sm tracking-widest">
       <h1 className="font-bold text-md text-white">STARBUCKS® REWARDS</h1>
     </div>
   );
