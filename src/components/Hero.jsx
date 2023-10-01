70966ceec19e8eff455e1d27ea0b3579231aaa3c
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-01 22:17:03 +0200

    imagen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,26 @@
 const Hero = () => {
   return (
-    <div className="hero flex items-center justify-start px-10 pt-20">
-      <div className="text-left py-5 w-[350px] flex flex-col gap-6 items-start text-lg font-semibold">
-        <h2 className="text-4xl tracking-wide font-semibold leading-snug w-[260px]">
-          FREE COFFEE IS A TAP AWAY
-        </h2>
-        <p>Join now to start earning Rewards.</p>
-        <button className="border border-sb-green bg-sb-green text-white font-semibold rounded-3xl px-4 py-1 hover:bg-sb-green2 transation-all">
-          Join now
-        </button>
-        <p className="small">
-          Or <a href="/" className="underline hover:no-underline">join in the app</a> for the best experience
-        </p>
+    <main className="flex">
+      <div className="hero flex items-center justify-between px-10 pt-20">
+        <div className="text-left py-5 w-[350px] flex flex-col gap-6 items-start text-lg font-semibold">
+          <h2 className="text-4xl tracking-wide font-semibold leading-snug w-[260px]">
+            FREE COFFEE IS A TAP AWAY
+          </h2>
+          <p>Join now to start earning Rewards.</p>
+          <button className="border border-sb-green bg-sb-green text-white font-semibold rounded-3xl px-4 py-1 hover:bg-sb-green2 transation-all">
+            Join now
+          </button>
+          <p className="small">
+            Or{" "}
+            <a href="/" className="underline hover:no-underline">
+              join in the app
+            </a>{" "}
+            for the best experience
+          </p>
+        </div>
       </div>
-    </div>
+      <img src="https://moneysavingmom.com/wp-content/uploads/2023/07/Screen-Shot-2023-07-10-at-10.31.09-PM-768x591.png" />
+    </main>
   );
 };
 
