978d021567fb7fa8290644aa84a3f9fc0a02eab1
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-02 13:07:23 +0200

    map images

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -1,21 +1,28 @@
+import { useState } from "react";
 import { PRICE_LIST } from "../constants";
 
 const Prices = () => {
+  const [selectedPriceIndex, setSelectedPriceIndex] = useState(null);
+
+  const handlePriceClick = (index) => {
+    setSelectedPriceIndex(index);
+  };
+
   return (
     <main>
       <div className="flex flex-col justify-center items-center px-20 pt-10 pb-4 text-center bg-sb-palid-green">
         <div className="w-full flex flex-col items-center justify-between h-full gap-16">
           <h2 className="font-bold text-3xl">Get your favourites for free</h2>
           <div className="flex gap-12 justify-center items-center">
-            {PRICE_LIST.map((price) => (
-              <a
-                href="#"
+            {PRICE_LIST.map((price, index) => (
+              <button
                 key={price.number}
                 className="text-3xl font-semibold flex items-baseline gap-1"
+                onClick={() => handlePriceClick(index)}
               >
                 {price.number}
                 <span className="text-base text-amber-600">★</span>
-              </a>
+              </button>
             ))}
           </div>
         </div>
@@ -23,11 +30,28 @@ const Prices = () => {
 
       <div className="flex gap-14 w-full bg-sb-light-green py-12 justify-center items-center">
         <div>
-          <img src={PRICE_LIST[0].image} alt="" width={360} />
+          <img
+            src={
+              selectedPriceIndex !== null
+                ? PRICE_LIST[selectedPriceIndex].image
+                : ""
+            }
+            alt=""
+            width={360}
+            className={`fade ${selectedPriceIndex !== null ? 'visible' : 'invisible'}`}
+          />
         </div>
-        <div className="flex flex-col gap-5">
-          <h3 className="text-2xl font-semibold">{PRICE_LIST[0].title}</h3>
-          <p className="w-[300px] text-base">{PRICE_LIST[0].subtitle}</p>
+        <div className="flex flex-col gap-5 w-[300px]">
+          <h3 className="text-2xl font-semibold">
+            {selectedPriceIndex !== null
+              ? PRICE_LIST[selectedPriceIndex].title
+              : ""}
+          </h3>
+          <p className=" text-base">
+            {selectedPriceIndex !== null
+              ? PRICE_LIST[selectedPriceIndex].subtitle
+              : ""}
+          </p>
         </div>
       </div>
     </main>
