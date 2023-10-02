6da0b5a56c3e2b3f17f8ae1d91775ddedea5457f
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-02 12:39:20 +0200

    prices

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { PRICE_LIST } from "../constants";
 
 const Prices = () => {
-  return <div>Prices</div>;
+  return (
+    <div className="flex flex-col justify-center items-center px-20 pt-16 pb-4 text-center gap-12 bg-sb-palid-green">
+      <div className="w-full flex flex-col items-center justify-between h-full gap-12">
+        <h2 className="font-bold text-3xl">Get your favourites for free</h2>
+        <div className="flex gap-12 justify-center items-center">
+          {PRICE_LIST.map((price) => (
+            <h3
+              key={price.number}
+              className="text-3xl font-semibold flex items-baseline gap-1"
+            >
+              {price.number}
+              <span className="text-base text-amber-600">★</span>
+            </h3>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Prices;
