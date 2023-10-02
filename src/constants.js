3afaeea5a0ecbd50aa43a884b44b4e01c2d0702f
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-02 12:22:51 +0200

    constants

---
+++ src/constants.js
@@ -0,0 +1,38 @@
+export const PRICE_LIST = [
+  {
+    number: 25,
+    title: "Customize your drink",
+    subtitle:
+      "Make your drink just right with an extra espresso shot, dairy substitute or a dash of your favourite syrup.",
+    image: "https://www.starbucks.ca/weblx/images/rewards/reward-tiers/025.png",
+  },
+  {
+    number: 100,
+    title: "Brewed hot coffee, bakery item or hot tea",
+    subtitle:
+      "Treat yourself to an iced coffee, buttery croissant, bag of chips and more.",
+    image: "https://www.starbucks.ca/weblx/images/rewards/reward-tiers/100.png",
+  },
+  {
+    number: 200,
+    title: "Handcrafted drink, hot breakfast or parfait",
+    subtitle:
+      "Turn good mornings great with a delicious handcrafted drink of your choice, breakfast sandwich, snack box or oatmeal on us.",
+    image: "https://www.starbucks.ca/weblx/images/rewards/reward-tiers/200.png",
+  },
+  {
+    number: 300,
+    title: "Lunch sandwich or at-home coffee",
+    subtitle:
+      "Enjoy a PM pick-me-up with a lunch sandwich or a bag of coffee—including Starbucks VIA Instant®.",
+    image:
+      "https://www.starbucks.ca/weblx/images/rewards/reward-tiers/300-ca.png",
+  },
+  {
+    number: 400,
+    title: "Select merchandise or at-home coffee",
+    subtitle:
+      "Take home a signature cup, drink tumbler or your choice of coffee merch up to $20.",
+    image: "https://www.starbucks.ca/weblx/images/rewards/reward-tiers/400.png",
+  },
+];
